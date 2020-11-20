392bd74053139886977ef6dca4cc6288fdd2ac04
vikasreddy636 <73032093+vikasreddy636@users.noreply.github.com>
2020-11-20 21:46:09 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,69 @@
+import multiprocessing
+import random
+import string
+import pandas as pd
+import time
+import datetime
+a = int(input("Enter 1st num -> "))
+b = int(input("Enter 2nd num -> "))
+sums = a+b
+seconds =[]
+values=[]
+timestamp = []
+a = time.monotonic()
+def sender(conn, msgs):
+    """
+    function to send messages to other end of pipe
+    """
+    for msg in msgs:
+        conn.send(msg)
+        print("Sent the message to: {}".format(msg))
+    conn.close()
+ 
+def receiver(conn):
+    """
+    function to print the messages received from other
+    end of pipe
+    """
+    while 1:
+        msg = conn.recv()
+        if msg == "z":
+            break
+        print("Received the message: {}".format(msg))
+while 1:
+    b = time.monotonic()
+    ct = str(datetime.datetime.now())
+    #ct = ct[0:19]
+    msgs = random.choice(string.ascii_letters)
+    if((msgs>='a' and msgs<= 'y') or (msgs>='A' and msgs<='Y')):
+        sums+=1
+        seconds.append(str(b-a)[0:1])
+        values.append(sums)
+        timestamp.append(ct)
+        print(sums)
+    if(msgs == 'z' or msgs == 'Z'):
+        df = pd.DataFrame.from_dict({'TimeStamp':timestamp,'Sum Value':values})
+        df.to_excel('exported.xlsx', header=True, index=False)
+        break
+        print("Received the message: {}".format(msg))
+if __name__ == "__main__":
+    # messages to be sent
+    msgs = random.choice(string.ascii_letters)
+ 
+    # creating a pipe
+    parent_conn, child_conn = multiprocessing.Pipe()
+ 
+    # creating new processes
+    p1 = multiprocessing.Process(target=sender, args=(parent_conn,msgs))
+    p2 = multiprocessing.Process(target=receiver, args=(child_conn,))
+ 
+    # running processes
+    p1.start()
+    p2.start()
+ 
+    # wait until processes finish
+    p1.join()
+    p2.join()
+    quit()
+    
+
